0610933abec278fe339eac954086600c3c061fe4
Helena Nevskaya <helena.nevskaya@ya.ru>
2016-10-31 14:47:49 +0300

    added paid hours and delay hours in params

--- dev/mt.sql
+++ dev/mt.sql
@@ -16,7 +16,6 @@ CREATE TABLE public.xmp_subscriptions (
     last_success_date TIMESTAMP WITHOUT TIME ZONE DEFAULT now(),
     id_service INTEGER NOT NULL DEFAULT 0,
     country_code INTEGER NOT NULL DEFAULT 0,
-    id_subscriber INTEGER NOT NULL DEFAULT 0,
     created_at TIMESTAMP WITHOUT TIME ZONE DEFAULT now(),
     msisdn CHARACTER VARYING(32),
     operator_code INTEGER NOT NULL DEFAULT 0,
@@ -26,40 +25,33 @@ CREATE TABLE public.xmp_subscriptions (
     result SUBSCRIPTION_STATUS NOT NULL DEFAULT ''::subscription_status,
     keep_days INTEGER NOT NULL DEFAULT 10,
     last_pay_attempt_at TIMESTAMP WITHOUT TIME ZONE NOT NULL DEFAULT now(),
-    paid_hours INTEGER NOT NULL DEFAULT 1,
+    paid_hours INTEGER NOT NULL,
+    delay_hours INTEGER NOT NULL,
     tid CHARACTER VARYING(127) NOT NULL DEFAULT ''
 );
+CREATE TYPE transaction_result AS ENUM ('failed', 'sms', 'paid', 'retry_failed', 'retry_paid', 'rejected', 'past');
+
 CREATE TABLE public.xmp_transactions (
     id serial,
     created_at TIMESTAMP WITHOUT TIME ZONE DEFAULT now(),
     msisdn CHARACTER VARYING(32),
     country_code INTEGER,
     id_service INTEGER,
-    status INTEGER,
-    operator_code INTEGER,
-    id_subscription INTEGER,
-    id_content INTEGER,
+    operator_code INTEGER NOT NULL DEFAULT 0,
+    id_subscription INTEGER NOT NULL DEFAULT 0,
     operator_token CHARACTER VARYING(511) NOT NULL,
     price INTEGER NOT NULL,
     result TRANSACTION_RESULT NOT NULL,
     id_campaign INTEGER NOT NULL DEFAULT 0
 );
--- transactions table
-CREATE TYPE transaction_result AS ENUM ('failed', 'sms', 'paid', 'retry_failed', 'retry_paid', 'rejected', 'past');
-ALTER TABLE xmp_transactions ADD COLUMN result transaction_result NOT NULL;
-UPDATE xmp_transactions SET result = 'past' WHERE created_at < now();
-ALTER TABLE xmp_transactions ADD COLUMN operator_token VARCHAR(511) NOT NULL;
-ALTER TABLE xmp_transactions ADD COLUMN price int NOT NULL;
-ALTER TABLE xmp_transactions ADD COLUMN id_campaign INT NOT NULL DEFAULT 0;
-
 CREATE TABLE xmp_retries (
     id serial,
     tid CHARACTER VARYING(127) NOT NULL DEFAULT '',
     created_at TIMESTAMP NOT NULL DEFAULT now(),
     last_pay_attempt_at TIMESTAMP NOT NULL DEFAULT NOW(),
     attempts_count INT NOT NULL DEFAULT 1,
-    keep_days INT NOT NULL DEFAULT 10,
-    delay_hours INT NOT NULL DEFAULT 24,
+    keep_days INT NOT NULL DEFAULT 0,
+    delay_hours INT NOT NULL DEFAULT 0,
     msisdn VARCHAR(32) NOT NULL,
     operator_code INTEGER NOT NULL,
     country_code INTEGER NOT NULL,
