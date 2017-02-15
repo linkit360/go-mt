846aa16a93a000a7d1dcb06cd5adad281c9389ff
Helena Nevskaya <helena.nevskaya@ya.ru>
2017-02-15 11:58:49 +0300

    qrtech handle delivery notifications

--- src/service/1qrtech.go
+++ src/service/1qrtech.go
@@ -22,7 +22,9 @@ type qrtech struct {
 	activeSubscriptions    *activeSubscriptions
 	shedulledSubscriptions *sync.WaitGroup
 	MOCh                   <-chan amqp_driver.Delivery
+	DNCh                   <-chan amqp_driver.Delivery
 	MOConsumer             *amqp.Consumer
+	DNConsumer             *amqp.Consumer
 }
 
 type QRTechConfig struct {
@@ -34,6 +36,7 @@ type QRTechConfig struct {
 	MCC             string                          `yaml:"mcc"`
 	CountryCode     int64                           `yaml:"country_code" default:"66"`
 	NewSubscription queue_config.ConsumeQueueConfig `yaml:"new"`
+	DN              queue_config.ConsumeQueueConfig `yaml:"dn"`
 	Charge          string                          `yaml:"charge" default:"qrtech_mt"`
 	Periodic        PeriodicConfig                  `yaml:"periodic"`
 }
@@ -75,6 +78,30 @@ func initQRTech(
 		log.Debug("new subscription disabled")
 	}
 
+	if qrTechConf.DN.Enabled {
+		if qrTechConf.DN.Name == "" {
+			log.Fatal("empty queue name dn")
+		}
+		qr.DNConsumer = amqp.NewConsumer(
+			consumerConfig,
+			qrTechConf.DN.Name,
+			qrTechConf.DN.PrefetchCount,
+		)
+		if err := qr.DNConsumer.Connect(); err != nil {
+			log.Fatal("rbmq consumer connect:", err.Error())
+		}
+		amqp.InitQueue(
+			qr.DNConsumer,
+			qr.DNCh,
+			qr.processDN,
+			qrTechConf.DN.ThreadsCount,
+			qrTechConf.DN.Name,
+			qrTechConf.DN.Name,
+		)
+	} else {
+		log.Debug("dn queue disabled")
+	}
+
 	if qrTechConf.Periodic.Enabled {
 		go func() {
 			for {
@@ -94,28 +121,38 @@ type QRTechMetrics struct {
 	AddToDBErrors  m.Gauge
 	AddToDbSuccess m.Gauge
 
+	DNDropped m.Gauge
+	DNErrors  m.Gauge
+	DNSuccess m.Gauge
+
 	GetPeriodicsDuration     prometheus.Summary
 	ProcessPeriodicsDuration prometheus.Summary
 }
 
-func (ch *qrtech) initMetrics() {
+func (qr *qrtech) initMetrics() {
 
-	ym := &QRTechMetrics{
-		MODropped:                m.NewGauge(appName, ch.conf.OperatorName, "mo_dropped", "yondu mo dropped"),
-		AddToDBErrors:            m.NewGauge(appName, ch.conf.OperatorName, "add_to_db_errors", "subscription add to db errors"),
-		AddToDbSuccess:           m.NewGauge(appName, ch.conf.OperatorName, "add_to_db_success", "subscription add to db success"),
+	qrm := &QRTechMetrics{
+		MODropped:                m.NewGauge(appName, qr.conf.OperatorName, "mo_dropped", "qrtech mo dropped"),
+		AddToDBErrors:            m.NewGauge(appName, qr.conf.OperatorName, "add_to_db_errors", "subscription add to db errors"),
+		AddToDbSuccess:           m.NewGauge(appName, qr.conf.OperatorName, "add_to_db_success", "subscription add to db success"),
+		DNDropped:                m.NewGauge(appName, qr.conf.OperatorName, "dn_dropped", "qrtech dn dropped"),
+		DNErrors:                 m.NewGauge(appName, qr.conf.OperatorName, "dn_errors", "qrtech dn errors"),
+		DNSuccess:                m.NewGauge(appName, qr.conf.OperatorName, "dn_success", "qrtech dn success"),
 		GetPeriodicsDuration:     m.NewSummary("get_periodics_duration_seconds", "get periodics duration seconds"),
 		ProcessPeriodicsDuration: m.NewSummary("process_periodics_duration_seconds", "process periodics duration seconds"),
 	}
 	go func() {
 		for range time.Tick(time.Minute) {
-			ym.MODropped.Update()
-			ym.AddToDBErrors.Update()
-			ym.AddToDbSuccess.Update()
+			qrm.MODropped.Update()
+			qrm.AddToDBErrors.Update()
+			qrm.AddToDbSuccess.Update()
+			qrm.DNDropped.Update()
+			qrm.DNErrors.Update()
+			qrm.DNSuccess.Update()
 		}
 	}()
 
-	ch.m = ym
+	qr.m = qrm
 }
 
 // ============================================================
@@ -208,4 +245,47 @@ func (qr *qrtech) publishCharge(priority uint8, r rec.Record) error {
 	}
 	svc.notifier.Publish(amqp.AMQPMessage{qr.conf.Charge, priority, body})
 	return nil
+}
+
+// ============================================================
+// dn
+
+type QRTechEventNotifyDN struct {
+	EventName string     `json:"event_name,omitempty"`
+	EventData rec.Record `json:"event_data,omitempty"`
+}
+
+func (qrTech *qrtech) processDN(deliveries <-chan amqp_driver.Delivery) {
+	for msg := range deliveries {
+		var r rec.Record
+
+		var e QRTechEventNotifyDN
+		if err := json.Unmarshal(msg.Body, &e); err != nil {
+			qrTech.m.DNDropped.Inc()
+
+			log.WithFields(log.Fields{
+				"error": err.Error(),
+				"msg":   "dropped",
+				"mo":    string(msg.Body),
+			}).Error("consume from " + qrTech.conf.DN.Name)
+			goto ack
+		}
+		r = e.EventData
+		if err := processResponse(&r, false); err != nil {
+			qrTech.m.DNErrors.Inc()
+			msg.Nack(false, true)
+			continue
+		}
+		qrTech.m.DNSuccess.Inc()
+	ack:
+		if err := msg.Ack(false); err != nil {
+			log.WithFields(log.Fields{
+				"mo":    msg.Body,
+				"error": err.Error(),
+			}).Error("cannot ack")
+			time.Sleep(time.Second)
+			goto ack
+		}
+
+	}
 }
