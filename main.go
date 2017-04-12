ab12fd8ebc54e26d945ddca6408875cac8fa6001
Helena Nevskaya <helena.nevskaya@ya.ru>
2017-04-12 11:40:13 +0300

    mobilink fixes

--- main.go
+++ main.go
@@ -10,7 +10,7 @@ import (
 
 func main() {
 	c := make(chan os.Signal, 3)
-	signal.Notify(c, os.Interrupt, syscall.SIGTERM, os.Kill)
+	signal.Notify(c, syscall.SIGINT, syscall.SIGTERM)
 	go func() {
 		<-c
 		src.OnExit()
